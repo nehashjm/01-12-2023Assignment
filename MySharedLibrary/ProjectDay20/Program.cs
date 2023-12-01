d7aa89638ed9ca17aefc8f26ae55fc78560b0f82
Nehash <nehash.j.m027@gmail.com>
2023-12-01 15:20:26 +0530

    Day20Assignment

---
+++ MySharedLibrary/ProjectDay20/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectDay20
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInput = "123";
+
+            // Example usage of utility methods
+            bool isNumber = MySharedLibrary.UtilityClass.IsNumber(userInput);
+            MySharedLibrary.UtilityClass.LogMessage($"User input: {userInput}, IsNumber: {isNumber}");
+            string encryptedText = MySharedLibrary.UtilityClass.Encrypt("SensitiveData");
+            Console.WriteLine(encryptedText);
+        }
+    }
+}
